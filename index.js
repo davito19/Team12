1ee53d9bd6c5b2eecbbcb13ae36bfdaa7fb0ad25
Paula Zapata <skailak@gmail.com>
2021-12-02 20:21:19 -0500

    Se encuentra la manera de enviar archivos tipo json. Falta hacer el testing y posibles correcciones.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 
 const calculo = require('./service/calculo')
+const otraCosa = require('./sprint-4/src')
 
 //app.use(express.json());
 
 app.use("/calculo", calculo)
+app.use("/loQueSea", otraCosa)
 
 const server = app.listen(3001, () =>{
     let port = server.address().port
--- sprint-4/src.js
+++ sprint-4/src.js
@@ -3,19 +3,24 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-app.get(/api/air-quality/aqi-ranges, (req, res)=> {
-console.log("aqi-ranges")
-res.status().json
+app.get('/api/air-quality/aqi_ranges', (req, res)=> {
+  console.log(req.body);
+  console.log(req.params);
+  var aqi_ranges = req.params.aqi_ranges
+  res.sendFile('api ${req.params.aqi_ranges}').type('json');
 });
 
-app.post(/api/air-quality/aqi-ranges, (req, res)=>{
-//range creation
+app.post('/api/air-quality/aqi_ranges', (req, res)=>{
+  console.log(req.body);
+  console.log(req.params);
+  res.sendFile('api ${req.params.aqi_ranges}').type('json')
 });
 
-app.get(
-    /api/air-quality/limits,
-    //retorna los limites
-);
-app.listen(3001)
+app.get('/api/air-quality/limits', (req, res)=>{
+  console.log(req.body);
+  console.log(req.params);
+  var limits = req.params.limits
+  res.sendFile('api ${req.params.limits}').type('json')
+});
 
 module.exports=app;
